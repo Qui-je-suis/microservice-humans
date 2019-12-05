8df8a7de5b5b91eb9bca0fbd77350a0d60bba5c6
cedcass <cassignac.cedric@orange.fr>
2019-12-05 19:36:25 +0100

    first commit

---
+++ src/main/java/mgb/mhumans/MhumansApplication.java
@@ -0,0 +1,11 @@
+package mgb.mhumans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhumansApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MhumansApplication.class, args);
+    }
+}
