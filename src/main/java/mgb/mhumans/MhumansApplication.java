81bc1d63a9d37772257301db82497d62efe348c3
cedcass <cassignac.cedric@orange.fr>
2019-12-07 20:26:28 +0100

    test restriction rules

--- src/main/java/mgb/mhumans/MhumansApplication.java
+++ src/main/java/mgb/mhumans/MhumansApplication.java
@@ -1,7 +1,7 @@
 /**
- *-------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
  * Copyright (C) 2019 QUI JE SUIS !!!. All rights reserved
- *-------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
  */
 package mgb.mhumans;
 
@@ -9,8 +9,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MhumansApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MhumansApplication.class, args);
-    }
+public class MhumansApplication
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(MhumansApplication.class, args);
+  }
 }
